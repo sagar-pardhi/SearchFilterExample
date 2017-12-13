dd5879c16949c279d2e39a358dc1cc6795a23ce9
sagar-pardhi <sgrpardhi007@gmail.com>
2017-12-13 20:12:19 +0530

    Created a POJO class contact.java

---
+++ app/src/main/java/com/android/example/searchfilterexample/Contact.java
@@ -0,0 +1,28 @@
+package com.android.example.searchfilterexample;
+
+/**
+ * Created by sagar on 13/12/17.
+ */
+
+public class Contact {
+
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
